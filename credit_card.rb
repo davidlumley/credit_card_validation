55372a8accada2a7d8354287575d4d0ddf54a854
David Lumley <david@davidlumley.com.au>
2012-03-17 18:57:53 +1000

    add check CreditCard types with type? method
    checks for the following types:
    AMEX
    Discover
    MasterCard
    Visa

--- credit_card.rb
+++ credit_card.rb
@@ -8,6 +8,28 @@ class CreditCard
   
   def type?
     
+    # begins with 3, followed by a 4 or 7, followed by 13 digits
+    if @number =~ /^3[47]\d{13}$/
+      'AMEX'
+      
+    # begins with 6011 followed by 12 digits
+    elsif @number =~ /^6011\d{12}$/
+      'Discover'
+      
+    # begins with a 3, followed by a digit between 1 and 5, followed by 14 digits
+    elsif @number =~ /^5[1-5]\d{14}$/
+      'MasterCard'
+    
+    # begins with a 4, followed by 12 digits, possibly followed by an additional 3
+    # add regex group for final digit because 1.8.7 doesn't like \d{3}?
+    elsif @number =~ /^4\d{12}(\d{3})?$/
+      'Visa'
+    
+    # doesn't match any of the above regex
+    else
+      'Unknown'
+    end
+    
   end
   
 end
