7476cf664ad4daa21b72e4c1d64bf87d36770485
David Lumley <david@davidlumley.com.au>
2012-03-17 22:00:22 +1000

    refactor CreditCard, remove puts from spec
    refactor type? from if/then/elsif pattern to more manageable
    iteration through types of cards
    
    refactor valid? to make method simpler
    
    remove extraneous puts in spec

--- credit_card.rb
+++ credit_card.rb
@@ -2,60 +2,38 @@ class CreditCard
   
   attr_accessor :number
   
+  CARD_TYPES = {
+    'AMEX'        => /^3[47]\d{13}$/,
+    'Discover'    => /^6011\d{12}$/,
+    'MasterCard'  => /^5[1-5]\d{14}$/,
+    'Visa'        => /^4\d{12}(\d{3})?$/
+  }
+  
   def initialize(number)
     @number = number.gsub(/\s+/, '')
   end
   
   def type?
-    
-    # begins with 3, followed by a 4 or 7, followed by 13 digits
-    if @number =~ /^3[47]\d{13}$/
-      'AMEX'
-      
-    # begins with 6011 followed by 12 digits
-    elsif @number =~ /^6011\d{12}$/
-      'Discover'
-      
-    # begins with a 3, followed by a digit between 1 and 5, followed by 14 digits
-    elsif @number =~ /^5[1-5]\d{14}$/
-      'MasterCard'
-    
-    # begins with a 4, followed by 12 digits, possibly followed by an additional 3
-    # add regex group for final digit because 1.8.7 doesn't like \d{3}?
-    elsif @number =~ /^4\d{12}(\d{3})?$/
-      'Visa'
-    
-    # doesn't match any of the above regex
-    else
-      'Unknown'
+    CARD_TYPES.each do |type, regex|
+      return type if @number =~ regex
     end
-    
+    'Unknown'
   end
   
-  ###
-  # Implementation of the Luhn algorithm
-  #
-  # reverse the number and for every even digit double it
-  # if the digit is greater than 9, add the seperate parts together (i.e. 10 -> 1 + 0)
-  # if the total sum of digits is divisible by 10, the card is valid
   def valid?
     i = 0
     sum = 0
-    @number.reverse.split('').map do |digit|
+    
+    @number.reverse.split('').map(&:to_i).map do |digit|
       i += 1
-      digit = digit.to_i
+      
       digit *= 2 if i.even?
-      if digit > 9
-        digit = digit.to_s.split('')
-        digit = digit[0].to_i + digit[1].to_i
-      end
+      digit -= 9 if digit > 9   # subtracting 9 is the same as adding the parts of a number greater than 9 but less than 20 together
+      
       sum += digit
     end
-    if sum % 10 == 0
-      true
-    else
-      false
-    end
+    
+    sum % 10 == 0 ? true : false
   end
   
 end
--- spec/credit_card_spec.rb
+++ spec/credit_card_spec.rb
@@ -6,7 +6,6 @@ describe CreditCard do
     
     before :each do
       @credit_card = CreditCard.new('1 2 3 4 5 6 7 8910')
-      puts @credit_card.number
     end
     
     it 'should strip spaces from the card number' do
