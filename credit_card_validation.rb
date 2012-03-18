48b525e61a2a2dff09349f920b4590987710de5a
David Lumley <david@davidlumley.com.au>
2012-03-18 16:55:23 +1000

    add validate functionality to credit_card_validation
    creates new instance of CreditCard and returns a
    string of format 'Type: Number Validity'

--- credit_card_validation.rb
+++ credit_card_validation.rb
@@ -1,4 +1,6 @@
 require 'lib/credit_card'
 
 def validate(number)
+  credit_card = CreditCard.new(number)
+  "#{credit_card.type?}: #{credit_card.number} (#{credit_card.valid? ? 'valid' : 'invalid'})"
 end
