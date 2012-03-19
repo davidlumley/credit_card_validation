34169794bf6032ef5623809f6a4cf43a8bbb5151
David Lumley <david@davidlumley.com.au>
2012-03-19 21:13:45 +1000

    add web frontend
    add a (very) basic web frontend to test cards
    - take a list of credit card values
    - show their validation status
    
    TODO: make it look nice/easy to use

--- app/app.rb
+++ app/app.rb
@@ -1,4 +1,22 @@
+require   'rubygems'
+require   'sinatra'
+require   'lib/credit_card.rb'
+
 module CreditCardValidation
   class App < Sinatra::Base
+    
+    get '/' do
+      erb :index
+    end
+    
+    post '/validate' do
+      card_numbers = params[:card_numbers]
+      @validation_status = Array.new
+      card_numbers.each do |number|
+        credit_card = CreditCard.new(number)
+        @validation_status.push credit_card.validation_status
+      end
+      erb :validation_status
+    end
   end
 end
